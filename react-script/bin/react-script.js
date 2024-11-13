53df74f9d269a881723b0f0e80e5bcd1ac7a0a9d
mm <562985990@qq.com>
2024-11-13 20:31:29 +0800

    查看源码

--- react-script/bin/react-script.js
+++ react-script/bin/react-script.js
@@ -2,9 +2,11 @@
 const path=require('path')
 const yargs = require('yargs')
 const chalk=require('chalk')
+const pkg = require('../package.json');
 const argv =yargs
   .command('start',"只是对命令的简短描述",(yargs)=>{
     console.log(chalk.red("start"));
   })
   .argv;
-console.log(argv);
+
+  const cmd = argv._[0]; // 获取非连词线开头的第一个参数,得到启动命令
