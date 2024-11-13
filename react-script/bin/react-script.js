a5570bddaac897e3ab94e3da9d68939bcb1a160b
17639638825 <562985990@qq.com>
2024-11-13 17:40:24 +0800

    提交

--- react-script/bin/react-script.js
+++ react-script/bin/react-script.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 const path=require('path')
-const yargs=require('yargs')
+const yargs = require('yargs')
 const chalk=require('chalk')
+const argv =yargs
+  .command('start',"只是对命令的简短描述",(yargs)=>{
+    console.log(chalk.red("start"));
+  })
+  .argv;
+console.log(argv);
